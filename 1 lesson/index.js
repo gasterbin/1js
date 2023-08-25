a05a8f9573f0db22bddc606956c9394268dfde3d
gasterbin <tergabi2007@gmail.com>
2023-08-25 21:19:53 +0300

    1

---
+++ 1 lesson/index.js
@@ -0,0 +1,47 @@
+//1
+let a = 10;
+let b = 5;
+console.log(a * b);
+
+//2
+let c = 6;
+let d = 2;
+console.log(c / d);
+
+//3
+let e = 3;
+let f = 4;
+console.log(e + f);
+
+//4
+let first = 11;
+let second = true;
+let third = 100;
+let fourth = 'java script';
+
+console.log(first, second, third, fourth);
+
+//5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
+/*-----------------------------------------------------------------------------------------------*/
+const userNumber = prompt('Your Number:');
+console.log(userNumber **2);
+
+const userFirstNumber = prompt('The first number:');
+const userSecondNumber = prompt ('The second number:');
+console.log((userFirstNumber + userSecondNumber) / 2);
+
+const userTime = prompt ('Minutes:');
+console.log (userTime * 60);
+
+const greeting = 'hello '
+let userName;
+userName = prompt('What is your name?');
+console.log(greeting + userName +  ' !');
